28fa88dd59edca15fd94d95f6d3610274c597f41
shokytooray <shoraygit@gmail.com>
2024-08-20 05:33:15 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ from importlib import import_module
 from highrise.__main__ import *
 
 # BOT SETTINGS
-bot_file_name = "xenoichi"
-bot_class_name = "xenoichi"
+bot_file_name = "main"
+bot_class_name = "bot"
 room_id = "66bad059afeca0c24b497205"
-bot_token ="d722fd437a1ce942d1efa288e88e8c0221033e1018c62fd8a8eb797859e6dcdf"
+bot_token = "6e6e5ac1f8c9fb0f917386e592d6ce3188538e1c15ccf4188079173cb72bf447"
 
 my_bot = BotDefinition(getattr(import_module(bot_file_name), bot_class_name)(), room_id, bot_token)
 
@@ -21,4 +21,3 @@ while True:
         print(f"An exception occurred: {e}")
         traceback.print_exc()
     time.sleep(5)
-
