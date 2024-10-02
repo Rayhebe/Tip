090ce2c9c50ea92237bdea57c9cc9fcf1b4d1db1
Rayhebe <hebekuranyi@gmail.com>
2024-10-02 18:09:32 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,49 +1,7 @@
-from flask import Flask
-from threading import Thread
-from highrise.__main__ import *
-import time
-
-
-class WebServer():
-
-  def __init__(self):
-    self.app = Flask(__name__)
-
-    @self.app.route('/')
-    def index() -> str:
-      return "Funcionando"
-
-  def run(self) -> None:
-    self.app.run(host='0.0.0.0', port=8084)
-
-  def keep_alive(self):
-    t = Thread(target=self.run)
-    t.start()
-
-
-class RunBot():
-  room_id = "66d2726b2e80dd1f614c4dbb"
-  bot_token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"
-  bot_file = "main"
-  bot_class = "Bot"
-
-  def __init__(self) -> None:
-    self.definitions = [
-        BotDefinition(
-            getattr(import_module(self.bot_file), self.bot_class)(),
-            self.room_id, self.bot_token)
-    ]  # More BotDefinition classes can be added to the definitions list
-
-  def run_loop(self) -> None:
-    while True:
-      try:
-        arun(main(self.definitions))
-
-      except Exception as e:
-        print("Error: ", e)
-        time.sleep(5)
+from main import Bot
+from asyncio import run as arun
 
 if __name__ == "__main__":
-  WebServer().keep_alive()
-
-  RunBot().run_loop()
+    room_id = "66d2726b2e80dd1f614c4dbb"  # Your room ID here
+    token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"  # Your token here
+    arun(Bot().run(room_id, token))
