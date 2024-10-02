7d57316a2ad0f2e57eab7a65197b824fa86c29a5
Rayhebe <hebekuranyi@gmail.com>
2024-10-02 19:43:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,4 @@ class Bot(BaseBot):
         except Exception as e:
             print(f"Error in on_chat: {e}")
 
-if __name__ == "__main__":
-    room_id = "66d2726b2e80dd1f614c4dbb"  # Your room ID here
-    token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"  # Your bot token here
-    arun(Bot().start(room_id, token))
+# No need to run the bot here, it will be managed by run.py
