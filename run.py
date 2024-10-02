6084a513626f28ce3bc935b46ce58bad2d5a7fa3
Rayhebe <hebekuranyi@gmail.com>
2024-10-02 21:30:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,49 @@
-from asyncio import run as arun
+from flask import Flask
+from threading import Thread
+from highrise.__main__ import *
 import time
-from main import Bot  # Import the Bot class from main.py
 
-class RunBot:
-    room_id = "66d2726b2e80dd1f614c4dbb"  # Room ID
-    bot_token = "202d7b11c3d00fe44e472d4c1b8ad9f4e3277e5fd03927d5cfab15532e9d8af8"  # Bot token
 
-    def run_loop(self) -> None:
-        while True:
-            try:
-                # Create an instance of the Bot and start it
-                arun(Bot().start(self.room_id, self.bot_token))
-            except Exception as e:
-                print(f"Error: {e}")
-                time.sleep(5)
+class WebServer():
+
+  def __init__(self):
+    self.app = Flask(__name__)
+
+    @self.app.route('/')
+    def index() -> str:
+      return "Funcionando"
+
+  def run(self) -> None:
+    self.app.run(host='0.0.0.0', port=8084)
+
+  def keep_alive(self):
+    t = Thread(target=self.run)
+    t.start()
+
+
+class RunBot():
+  room_id = "66d2726b2e80dd1f614c4dbb"
+  bot_token = "202d7b11c3d00fe44e472d4c1b8ad9f4e3277e5fd03927d5cfab15532e9d8af8"
+  bot_file = "main"
+  bot_class = "Bot"
+
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
+
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
+
+      except Exception as e:
+        print("Error: ", e)
+        time.sleep(5)
 
 if __name__ == "__main__":
-    RunBot().run_loop()
+  WebServer().keep_alive()
+
+  RunBot().run_loop()
