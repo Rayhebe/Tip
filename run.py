f0904cd0af72506d5f0035f72bd45d403b579d3d
Rayhebe <hebekuranyi@gmail.com>
2024-10-02 19:10:05 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,30 @@
-import streamlit as st
-from main import Bot
-from functions.webserver import keep_alive
+from highrise import BaseBot, User, SessionMetadata
+from highrise.models import Position
 from asyncio import run as arun
+import importlib.util
+import time
 
-# Set up the Streamlit interface
-st.title("Highrise Game Bot")
-st.write("Starting the bot...")
+class RunBot():
+    room_id = "66d2726b2e80dd1f614c4dbb"  # Room ID
+    bot_token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"  # Bot token
+    bot_file = "main"
+    bot_class = "Bot"
+
+    def __init__(self) -> None:
+        self.definitions = [
+            BotDefinition(
+                getattr(import_module(self.bot_file), self.bot_class)(),
+                self.room_id, self.bot_token)
+        ]
+
+    def run_loop(self) -> None:
+        while True:
+            try:
+                arun(main(self.definitions))
+
+            except Exception as e:
+                print("Error: ", e)
+                time.sleep(5)
 
 if __name__ == "__main__":
-    keep_alive()  # Start the web server to keep the bot alive
-    room_id = "66d2726b2e80dd1f614c4dbb"  # Your room ID here
-    token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"  # Your bot token here
-
-    try:
-        st.write("Running bot...")
-        arun(Bot().start(room_id, token))  # Make sure this line matches the method defined in main.py
-    except Exception as e:
-        st.error(f"An error occurred while running the bot: {e}")
+    RunBot().run_loop()
