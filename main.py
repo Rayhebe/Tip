cabf99a34b6286f0f28cdab72a2c8470eb66f208
Rayhebe <hebekuranyi@gmail.com>
2024-10-02 18:54:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ class Bot(BaseBot):
 if __name__ == "__main__":
     room_id = "66d2726b2e80dd1f614c4dbb"  # Your room ID here
     token = "432f23df3fc5076fe6c95ade994a533c9d473ecdb56acc31346899a94d6aaa6d"  # Your bot token here
-    arun(Bot().run(room_id, token))
+    arun(Bot().start(room_id, token))  # Change this line to call the correct method
