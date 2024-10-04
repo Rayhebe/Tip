bf4eb509e82dae161513d8edc13942b4a914009f
Rayhebe <hebekuranyi@gmail.com>
2024-10-04 08:24:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,19 +44,6 @@ class Bot(BaseBot):
 
     async def on_chat(self, user: User, message: str) -> None:
         try:
-            _bid = "66be9396fdcc1589bbf8f297"  # Bot user ID
-            _rid = "66d2726b2e80dd1f614c4dbb"  # Room ID
-            _id = f"1_on_1:{_bid}:{user.id}"
-            _idx = f"1_on_1:{user.id}:{_bid}"
-
-            # Check if the message is private and from RayMG or sh1n1gam1699
-            if user.username.lower() in ["raymg", "sh1n1gam1699"]:
-                if message.lower().startswith(f"@mghbot"):
-                    user_message = message[len(f"@mghbot "):].strip()
-                    # Broadcast the private message to the room
-                    await self.highrise.chat(f"{user.username}: {user_message}")
-        except Exception as e:
-            print(f"Error in on_chat: {e}")
 
         if message.lower().startswith("-tipall ") and user.username == "RayMG":
               parts = message.split(" ")
